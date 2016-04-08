77d1ce0f18a0f888dd86fb2c0aaa8d447f658c1d
Di Wu <allwu@falcon-computing.com>
2016-04-07 18:07:49 -0700

    Refined scripts extracted from Geschwind's group

--- scripts/globals.sh
+++ scripts/globals.sh
@@ -1,105 +1,23 @@
-server="bmap" # either "hoffman" or "bmap"
-
-if [ $server = "hoffman" ]; then
-# NGS Home Directory
-ngs_dir="/u/project/eeskin/geschwind/NGS"
+# Directories
+data_dir=/space/scratch/genome
+fastq_dir=$data_dir/fastq
+sam_dir=$data_dir/sam
+bam_dir=$data_dir/bam
+rpt_dir=$data_dir/rpt
+vcf_dir=$data_dir/vcf
+ref_dir=$data_dir/ref
+tools_dir=/space/common_mnt/tools
 
 # Reference Genome Info
-ref_genome_dir="$ngs_dir/reference_genome"
-ref_genome="$ref_genome_dir/hs37d5_normalized.fa"
-db138_SNPs="$ref_genome_dir/dbsnp_138.b37.vcf"
-g1000_indels="$ref_genome_dir/1000G_phase1.indels.b37.vcf"
-g1000_gold_standard_indels="$ref_genome_dir/Mills_and_1000G_gold_standard.indels.b37.vcf"
-hapmap="$ref_genome_dir/hapmap_3.3.b37.vcf"
-g1000_omni2_5="$ref_genome_dir/1000G_omni2.5.b37.vcf"
+ref_genome=$ref_dir/human_g1k_v37.fasta
+db138_SNPs=$ref_dir/dbsnp_138.b37.vcf
+g1000_indels=$ref_dir/1000G_phase1.indels.b37.vcf
+g1000_gold_standard_indels=$ref_dir/Mills_and_1000G_gold_standard.indels.b37.vcf
 
-# Directories
-ngs_raw_data_dir="$ngs_dir/NGS_raw_data"
-ngs_code_dir="$ngs_dir/NGS_code"
-ngs_sample_info_dir="$ngs_dir/NGS_sample_info"
-
-# Binary application calls
-local_apps_dir="/u/local/apps"
-geschwind_programs_dir="/u/nobackup/dhg/Software"
-anaconda_python_call="$geschwind_programs_dir/anaconda/bin/python"
-bamtools_call="$geschwind_programs_dir/bamtools/bin/bamtools"
-bedtools_call="$local_apps_dir/bedtools/2.23.0/gcc-4.4.7/bin/bedtools"
-bedtools_multi_intersect_call="$local_apps_dir/bedtools/2.23.0/gcc-4.4.7/bin/multiIntersectBed"
-bwa_call="$local_apps_dir/bwa/current/bwa"
-fastqc_call="/u/home/n/nkwok/FastQC/fastqc"
-gatk_call="$local_apps_dir/gatk/3.3-0/GenomeAnalysisTK.jar"
-picard_markdups_call="$local_apps_dir/picard-tools/current/MarkDuplicates.jar"
-java_call="/usr/bin/java"
-samtools_call="$geschwind_programs_dir/samtools-1.2/samtools"
-
-# .bed files
-QC_supplemental_files_dir="$ngs_code_dir/QC_supplemental_input_files"
-genome_no_gaps_bedfile="$QC_supplemental_files_dir/genome.noGapregions.noChr.bed"
-genome_exon_bedfile="$QC_supplemental_files_dir/gencode.v19.annotation.protein-coding.exons.noChr.bed"
-exome_no_gaps_bedfile="$QC_supplemental_files_dir/SeqCap_EZ_Exome_v2_noCHR.bed"
-exome_exon_bedfile="$QC_supplemental_files_dir/Exome_GencodeExon_Overlap_noCHR.bed"
-
-
-function construct_qsub {
-	qsub_call="qsub -cwd -N $1 -l h_data=2G,h_rt=08:00:00,highp -pe shared 8 -e $2 -o $2"
-
-	if [ $4 ]; then
-		qsub_call="$qsub_call -hold_jid $4"
-	fi
-	qsub_call="$qsub_call $3"
-	eval $qsub_call
-}
-
-fi
-
-if [ $server = "bmap" ]; then
-
-# Home Directory
-ngs_dir="/ifs/collabs/geschwind/NGS"
-
-# Reference Genome Info
-ref_genome_dir="$ngs_dir/reference_genome"
-ref_genome="$ref_genome_dir/hs37d5_normalized.fa"
-db138_SNPs="$ref_genome_dir/dbsnp_138.b37.vcf"
-g1000_indels="$ref_genome_dir/1000G_phase1.indels.b37.vcf"
-g1000_gold_standard_indels="$ref_genome_dir/Mills_and_1000G_gold_standard.indels.b37.vcf"
-hapmap="$ref_genome_dir/hapmap_3.3.b37.vcf"
-g1000_omni2_5="$ref_genome_dir/1000G_omni2.5.b37.vcf"
-
-# Directories
-ngs_raw_data_dir="$ngs_dir/NGS_raw_data"
-ngs_code_dir="$ngs_dir/NGS_code"
-ngs_sample_info_dir="$ngs_dir/NGS_sample_info"
-## JUST FOR TESTING
-ngs_raw_data_dir="/ifshome/nkwok/scripts"
-ngs_sample_info_dir="/ifshome/nkwok/scripts/sample_info"
-# Binary application calls
-geschwind_programs_dir="/ifs/collabs/geschwind/programs"
-anaconda_python_call="/ifshome/nkwok/anaconda/bin/python"
-bamtools_call="$geschwind_programs_dir/bamtools/bamtools/bin/bamtools"
-bedtools_call="$geschwind_programs_dir/bedtools/bedtools2/bin/bedtools"
-bedtools_multi_intersect_call="$geschwind_programs_dir/bedtools/bedtools2/bin/multiIntersectBed"
-bwa_call="$geschwind_programs_dir/bwa/bwa-0.7.10/bwa"
-fastqc_call="$geschwind_programs_dir/fastqc/fastqc/fastqc"
-gatk_call="$geschwind_programs_dir/gatk/GenomeAnalysisTK.jar"
-picard_markdups_call="$geschwind_programs_dir/picard/picard-tools-1.119/MarkDuplicates.jar"
-java_call="/usr/local/java/jdk1.7.0_45/bin/java"
-samtools_call="/usr/local/loniWorkflows/Bioinformatics/samtools-0.1.19/samtools"
-
- # .bed files
- QC_supplemental_files_dir="$ngs_code_dir/QC_supplemental_input_files"
- genome_no_gaps_bedfile="$QC_supplemental_files_dir/genome.noGapregions.noChr.bed"
- genome_exon_bedfile="$QC_supplemental_files_dir/gencode.v19.annotation.protein-coding.exons.noChr.bed"
- exome_no_gaps_bedfile="$QC_supplemental_files_dir/SeqCap_EZ_Exome_v2_noCHR.bed"
- exome_exon_bedfile="$QC_supplemental_files_dir/Exome_GencodeExon_Overlap_noCHR.bed"
-
-function construct_qsub {
-	qsub_call="qsub -cwd -N $1 -l h_vmem=8.625G,h_stack=8.625 -e $2 -o $2 -q long.q"
-	if [ $4 ]; then
-		qsub_call="$qsub_call -hold_jid $4"
-	fi
-	qsub_call="$qsub_call $3"
-	eval $qsub_call
-}
-
-fi
+# Tools
+BWA=$tools_dir/bwa/bwa
+BAMTOOL=
+SAMTOOLS=$tools_dir/samtools-1.3/samtools
+GATK=$tools_dir/gatk-3.5/GenomeAnalysisTK.jar
+PICARD=$tools_dir/picard-tools-1.141/picard.jar
+JAVA=/tools/jdk1.7.0_80/bin/java
---
+++ scripts/pipeline.sh
@@ -0,0 +1,191 @@
+#############################################################################################
+## This script generates gVCF files from a specified in a input file 
+## (c) Di Wu 04/07/2016
+#############################################################################################
+
+#!/bin/bash
+
+# Import global variables
+source globals.sh
+
+if [[ $# -lt 1 ]]; then
+  echo "USAGE: $0 <sample id> [1..5]"
+  exit 1;
+fi
+
+sample_id=$1
+RG_ID=SEQ01
+platform=ILLUMINA
+library=HUMsgR2AQDCAAPE
+
+shift
+
+# dictionary to enable selective stages
+declare -A do_stage
+if [[ $# -gt "0" ]]; then
+  do_stage=( ["1"]="0" ["2"]="0" ["3"]="0" ["4"]="0" ["5"]="0" )
+  for i in $@; do 
+    do_stage["$i"]=1
+  done
+else
+  do_stage=( ["1"]="1" ["2"]="1" ["3"]="1" ["4"]="1" ["5"]="1" )
+fi
+
+set -x
+
+# Step 1: BWA alignment
+# - Input: pair_end fastq files: ${sample_id}_1.fastq.gz ${sample_id}_2.fastq.gz
+# - Output: ${sample_id}.sam
+
+if [[ "${do_stage["1"]}" == "1" ]]; then
+  # Check if input file exist
+  if [ ! -f $fastq_dir/${sample_id}_1.fastq ] || [ ! -f $fastq_dir/${sample_id}_1.fastq ]; then
+    echo "Cannot find input fastq files"
+    exit 1
+  fi
+  start_ts=$(date +%s)
+  $BWA mem -M -t 24 $ref_genome \
+      -R "@RG\tID:$RG_ID\tSM:$sample_id\tPL:$platform\tLB:$library" \
+      $fastq_dir/${sample_id}_1.fastq \
+      $fastq_dir/${sample_id}_2.fastq > $sam_dir/${sample_id}.sam
+  end_ts=$(date +%s)
+  echo "#1 BWA mem finishes in $((end_ts - start_ts))s"
+fi
+
+# Step 2: Samtools Sort
+# - Input: ${sample_id}.sam
+# - Output: sorted ${sample_id}.bam
+if [[ "${do_stage["2"]}" == "1" ]]; then
+  if [ ! -f $sam_dir/${sample_id}.sam ]; then
+    echo "Cannot find $sam_dir/${sample_id}.sam"
+    exit 1
+  fi
+  start_ts=$(date +%s)
+  cat $sam_dir/${sample_id}.sam | $SAMTOOLS view -S -b -u - | $SAMTOOLS sort -o $bam_dir/${sample_id}.bam
+  end_ts=$(date +%s)
+  echo "#2 Samtools sort finishes in $((end_ts - start_ts))s"
+fi
+
+# These steps can be omitted
+# Step 3 (REMOVED): Check BAM file
+#echo "$SAMTOOL view -H $bam_dir/${sample_id}.bam > tmp.out"
+#rm -f tmp.out err
+
+# Step 4 (REMOVED): 
+#$BAMTOOL merge \
+#    -list $input_dir/$SampleID/Logs/list_BAMs_tomerge.txt \
+#    -out $input_dir/$SampleID/$SampleID.merged.bam
+
+
+# Step 3: Mark Duplicate
+# - Input: sorted ${sample_id}.bam
+# - Output: duplicates-removed ${sample_id}.markdups.bam
+if [[ "${do_stage["3"]}" == "1" ]]; then
+  if [ ! -f $bam_dir/${sample_id}.bam ]; then
+    echo "Cannot find $bam_dir/${sample_id}.bam"
+    exit 1
+  fi
+  start_ts=$(date +%s)
+  $JAVA -XX:+UseSerialGC -Xmx2g -jar $PICARD \
+      MarkDuplicates \
+      TMP_DIR=/tmp COMPRESSION_LEVEL=5 \
+      INPUT=$bam_dir/${sample_id}.bam \
+      OUTPUT=$bam_dir/${sample_id}.markdups.bam \
+      METRICS_FILE=$rpt_dir/${sample_id}.bam.dups_stats \
+      REMOVE_DUPLICATES=true ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT
+  end_ts=$(date +%s)
+  echo "#3-1 Picard mark duplicate finishes in $((end_ts - start_ts))s"
+
+  start_ts=$(date +%s)
+  $SAMTOOLS index $bam_dir/${sample_id}.markdups.bam
+  end_ts=$(date +%s)
+  echo "#3-2 Samtools index finishes in $((end_ts - start_ts))s"
+fi
+
+# Step 4: GATK BaseRecalibrate
+# - Input: ${sample_id}.markdups.bam
+# - Output: recalibrated ${sample_id}.markdups.recal.bam
+if [[ "${do_stage["4"]}" == "1" ]]; then
+  if [ ! -f $bam_dir/${sample_id}.markdups.bam ]; then
+    echo "Cannot find $bam_dir/${sample_id}.markdups.bam"
+    exit 1
+  fi
+  # - first recalibrate the markdups.bam file and generate a report
+  start_ts=$(date +%s)
+  $JAVA -d64 -Xmx2g -jar $GATK \
+      -T BaseRecalibrator \
+      -R $ref_genome \
+      -I $bam_dir/${sample_id}.markdups.bam \
+      -knownSites $g1000_indels \
+      -knownSites $g1000_gold_standard_indels \
+      -knownSites $db138_SNPs \
+      -o $rpt_dir/${sample_id}.recalibration_report.grp
+  end_ts=$(date +%s)
+  echo "#4-1 BaseRecalibrator finishes in $((end_ts - start_ts))s"
+  
+  # - then use print reads to apply the calibration
+  start_ts=$(date +%s)
+  $JAVA -Xmx2g -jar $GATK \
+      -T PrintReads \
+      -R $ref_genome \
+      -I $bam_dir/${sample_id}.markdups.bam \
+      -BQSR $rpt_dir/${sample_id}.recalibration_report.grp \
+      -o $bam_dir/${sample_id}.markdups.recal.bam
+  end_ts=$(date +%s)
+  echo "#4-2 PrintReads finishes in $((end_ts - start_ts))s"
+  
+  # - then recalibrate again to generate another report
+  start_ts=$(date +%s)
+  $java_call -Xmx2g -jar $GATK \
+      -T BaseRecalibrator \
+      -R $ref_genome \
+      -I $bam_dir/${sample_id}.markdups.recal.bam \
+      -knownSites $g1000_indels \
+      -knownSites $g1000_gold_standard_indels \
+      -knownSites $db138_SNPs \
+      -o $rpt_dir/${sample_id}.postrecalibration_report.grp
+  end_ts=$(date +%s)
+  echo "#4-3 BaseRecalibrator finishes in $((end_ts - start_ts))s"
+  
+  # - finally calculate a covariate report
+  start_ts=$(date +%s)
+  $JAVA -d64 -Xmx2g -jar $GATK \
+      -T AnalyzeCovariates \
+      -R $ref_genome \
+      -before $input_dir/$SampleID/${SampleID}.recalibration_report.grp \
+      -after $input_dir/$SampleID/${SampleID}.postrecalibration_report.grp \
+      -plots $input_dir/$SampleID/Plots/${SampleID}.recalibration_plot.pdf
+  end_ts=$(date +%s)
+  echo "#4-4 AnalyzeCovariates finishes in $((end_ts - start_ts))s"
+  
+  start_ts=$(date +%s)
+  $SAMTOOLS index $bam_dir/${sample_id}.markdups.recal.bam
+  end_ts=$(date +%s)
+  echo "#4-5 Samtools index finishes in $((end_ts - start_ts))s"
+fi
+
+# Step 5: GATK HaplotypeCaller
+# - Input: ${sample_id}.markdups.recal.bam
+# - Output: per chromosome varients ${sample_id}_$chr.gvcf
+if [[ "${do_stage["5"]}" == "1" ]]; then
+  if [ ! -f $bam_dir/${sample_id}.markdups.recal.bam ]; then
+    echo "Cannot find $bam_dir/${sample_id}.markdups.recal.bam"
+    exit 1
+  fi
+  chr_list="$(seq 1 22) X Y MT"
+  for chr in $chr_list; do
+  start_ts=$(date +%s)
+  $JAVA -Xmx2g -jar $GATK \
+      -T HaplotypeCaller \
+      -R $ref_genome \
+      -I $bam_dir/${sample_id}.markdups.recal.bam \
+      --emitRefConfidence GVCF \
+      --variant_index_type LINEAR \
+      --variant_index_parameter 128000 \
+      -L $chr \
+      -o $vcf_dir/${sample_id}_${chr}.gvcf
+  end_ts=$(date +%s)
+  echo "#5 HaplotypeCaller on CH:$chr finishes in $((end_ts - start_ts))s"
+  done
+fi
+set +x
