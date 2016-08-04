f8d2e6912d1455564a5f2cd9c9089d6436ee4a87
yaohuFCS <yaohu@falcon-computing.com>
2016-08-04 08:05:15 -0700

    Minor modify on cancatGVCF.sh to compatible with the output of former stages

--- scripts/stage-worker/concatVCF.sh
+++ scripts/stage-worker/concatVCF.sh
@@ -20,7 +20,7 @@ else
 fi
 
 print_help() {
-  echo "USAGE: $0 -i <input_dir> -o <output>"
+  echo "USAGE: fcs-genome concat|concatGVCF -i <input_dir> -o <output>"
 }
 
 if [[ $# -lt 2 ]]; then
@@ -53,11 +53,22 @@ check_arg "-i" "input_vcf_dir"
 check_arg "-o" "output_dir" $(pwd)
 check_args
 
+# Get absolute file path
+readlink_check input_vcf_dir
+readlink_check output_dir
+readlink_check log_dir
+
+create_dir $output_dir
+create_dir $log_dir
+check_output_dir $output_dir
+check_output_dir $log_dir
+
 get_sample_id() {
   local input_dir=$1;
   local sample_id=`ls $input_dir/*.gvcf | sed -e 'N;s/^\(.*\).*\n\1.*$/\1\n\1/;D'`;
   local sample_id=$(basename $sample_id);
-  local sample_id=${sample_id::-4};
+  #local sample_id=${sample_id::-4};
+  local sample_id=${sample_id%%.*}
   echo $sample_id;
 }
 
@@ -74,10 +85,10 @@ vcf_sample_id=`get_sample_id $input_vcf_dir`
 input_vcf_list=
 chr_list="$(seq 1 22) X Y MT";
 for chr in $chr_list; do
-  input_vcf_list="$input_vcf_list ${vcf_sample_id}.chr${chr}.gvcf"
+  input_vcf_list="$input_vcf_list ${input_vcf_dir}/${vcf_sample_id}.chr${chr}.gvcf"
 done
 
-$BCFTOOLS concat $input_vcf_list -o $output_dir/${vcf_sample_id}.gvcf &
+$BCFTOOLS concat $input_vcf_list -o $output_dir/${vcf_sample_id}.gvcf &>$log_dir/concat.log &
 task_pid=$!
 wait "$task_pid"
 if [ "$?" -ne "0" ]; then
@@ -85,7 +96,7 @@ if [ "$?" -ne "0" ]; then
   exit 1;
 fi
 
-$BGZIP -c $output_dir/${vcf_sample_id}.gvcf > $output_dir/${vcf_sample_id}.gvcf.gz &
+$BGZIP -c $output_dir/${vcf_sample_id}.gvcf > $output_dir/${vcf_sample_id}.gvcf.gz &>>$log_dir/concat.log &
 task_pid=$!
 wait "$task_pid"
 if [ "$?" -ne "0" ]; then
@@ -96,7 +107,7 @@ fi
 # delete uncompressed gvcf file
 rm $output_dir/${vcf_sample_id}.gvcf
 
-$TABIX -p vcf $output_dir/${vcf_sample_id}.gvcf.gz &
+$TABIX -p vcf $output_dir/${vcf_sample_id}.gvcf.gz &>>$log_dir/concat.log &
 task_pid=$!
 wait "$task_pid"
 if [ "$?" -ne "0" ]; then
