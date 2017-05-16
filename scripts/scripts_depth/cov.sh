7cf77842ae789ba904c8a74d74043b615a3ce695
Niveda-S <nivedasundar05@gmail.com>
2017-05-16 15:36:56 -0700

    Updates to stats calculation

--- scripts/scripts_depth/cov.sh
+++ scripts/scripts_depth/cov.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-source $DIR/globals.sh.template
+PARENTDIR="$(dirname "$DIR")"
+source $PARENTDIR/globals.sh.template
 
-sample="Tumor"
+sample="$2"
 PATH_TO_BAM="$1"     #/pool/storage/diwu/annovar/LB-2907-TumorDNA.recal.bam
 CODING_REGION_PATH="/curr/niveda/coverage/FCS/hg19_coding_merge.bed"
 
@@ -15,24 +16,53 @@ if [ -d "$PATH_TO_BAM" ];then
     if [[ $file =~ bam$ ]];then
       #Ignore duplicates in BAM file | Calculate coverage values 
       samtools view -b -F 0x400 "$PATH_TO_BAM"/"$file" | "$BEDTOOLS" genomecov -ibam stdin -bga >> "${sample}_coverage.bed"
+      if [[ $? -ne 0 ]]; then
+        echo 'genomecov failed'
+        exit 1
+      fi
     fi
   done
-fi
 
 #If single file, then do coverage calculation for only that file
-if [ -f "$PATH_TO_BAM" ];then
-  samtools view -b -F 0x400 "$PATH_TO_BAM"/"$file" | "$BEDTOOLS" genomecov -ibam stdin -bga >> "${sample}_coverage.bed"
+elif [ -f "$PATH_TO_BAM" ];then
+  samtools view -b -F 0x400 "$PATH_TO_BAM" | "$BEDTOOLS" genomecov -ibam stdin -bga >> "${sample}_coverage.bed" 
+  if [[ $? -ne 0 ]]; then
+    echo 'genomecov failed'
+    exit 1
+  fi
+
+#If file of that name does not exist, quit
+else
+  echo "$PATH_TO_BAM is not valid"
+  exit 1
 fi
 
 #Filter to only coding regions	
-"$BEDTOOLS" intersect -loj -a "$CODING_REGION_PATH" -b "$sample"_coverage.bed > "${sample}_codingcov.bed"
+"$BEDTOOLS" intersect -loj -a "$CODING_REGION_PATH" -b "$sample"_coverage.bed > "${sample}_codingcov.bed" 
+if [[ $? -ne 0 ]]; then 
+  echo 'intersect failed'
+  exit 1
+fi
 
 #Coverage stats calculation
-perl cov_calculate.pl $sample
-
+perl cov_calculate.pl $sample 
+if [[ $? -ne 0 ]]; then
+  echo 'stats calculation failed'
+  exit 1
+fi
 
 sed -i 's/,$//' "${sample}_${sample}Coverage.csv"
+if [[ $? -ne 0 ]]; then
+  echo 'error manipulating stats file'
+  exit 1
+fi
 
 #Coverage stats plot
 python cov_graph.py $sample
- 
+if [[ $? -ne 0 ]]; then
+  echo 'error with plot computation'
+  exit 1
+fi
+
+#Remove intermediate files
+rm *.bed 
