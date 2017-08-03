78ed82b3073c85519fe24f990c4e1316219ea358
Niveda-S <nivedasundar05@gmail.com>
2017-08-03 15:23:37 -0700

    Create test_helper.bash

---
+++ tests/daily_tests/test_helper.bash
@@ -0,0 +1,13 @@
+#Reference
+ref_dir="/genome/ref"
+ref_genome="${ref_dir}/human_g1k_v37.fasta"
+db138_SNPs="${ref_dir}/dbsnp_138.b37.vcf"
+g1000_indels="${ref_dir}/1000G_phase1.indels.b37.vcf"
+g1000_gold_standard_indels="${ref_dir}/Mills_and_1000G_gold_standard.indels.b37.vcf"
+
+#Paths
+test_fastq="/curr/niveda/test_fastq"
+output="/curr/niveda/Testing/output"
+pipeline="/curr/niveda/pipeline/wgs.sh"
+
+BATS="/curr/niveda//bats/bin/bats"
