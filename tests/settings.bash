d4e47a457305da81305a71debabd5a31f2303c7a
Niveda-S <nivedasundar05@gmail.com>
2017-08-08 13:50:28 -0700

    Create settings.bash

---
+++ tests/settings.bash
@@ -0,0 +1,12 @@
+#Reference
+ref_dir="/genome/ref"
+ref_genome="${ref_dir}/human_g1k_v37.fasta"
+db138_SNPs="${ref_dir}/dbsnp_138.b37.vcf"
+g1000_indels="${ref_dir}/1000G_phase1.indels.b37.vcf"
+g1000_gold_standard_indels="${ref_dir}/Mills_and_1000G_gold_standard.indels.b37.vcf"
+
+#Paths
+test_fastq="/curr/niveda/Testing/fastq"
+output="/curr/niveda/Testing/output"
+BATS="/curr/niveda/bats/bats"
+fcs_genome="/curr/software/falcon-genome/latest/setup.sh"
