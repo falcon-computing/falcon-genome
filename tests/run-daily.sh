ea74cb48a87eb0861ab9ac95af9537528daca403
Niveda-S <nivedasundar05@gmail.com>
2017-08-14 14:05:17 -0700

    Update run-daily.sh

--- tests/run-daily.sh
+++ tests/run-daily.sh
@@ -3,16 +3,15 @@
 source settings.bash
 
 # Create directory
-mkdir $test_fastq
-mkdir $output
+mkdir -p $test_fastq
+mkdir -p $output
 
 # Download small FASTQ files from s3
 aws s3 sync s3://fcs-genome-data/falcon-genome-test/data/daily/fastq/ "$test_fastq"
 
 # Install BATS
-echo "$DIR"
-mkdir "${DIR}/env"
-mkdir "${DIR}/env/bats"
+mkdir -p "${DIR}/env"
+mkdir -p "${DIR}/env/bats"
 aws s3 sync s3://fcs-genome-data/falcon-genome-test/tools/BATS "${DIR}/env/bats"
 
 chmod 777 "$BATS"
@@ -20,5 +19,4 @@ chmod 777 $DIR/env/bats/libexec/*
 export PATH=$PATH:$DIR/env/bats/libexec
 
 # Run bats tests
-$BATS daily_tests/
-~            
+$BATS daily/
