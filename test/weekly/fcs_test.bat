3bb1db53e3db28f3524f14d69b1f2da220024db6
WenPing-falcon <pingwen@falcon-computing.com>
2017-08-31 22:55:49 -0700

    1. this bat can test tools' output now 2. add config file fcs_test_mode.sh to set test mode 3. new case can be written in a seperate bat file. The dir will be given to bats as arg. tools(/curr/wenp/Analysis/fcs-genome_test-env): sampleFastq: randomly get specified number data (1G, 2G, 10G...) bamdiff: compare two bams to get the common proportion vcfdiff: compre two vcf files to get the commom proportion

--- test/weekly/fcs_test.bat
+++ test/weekly/fcs_test.bat
@@ -3,11 +3,7 @@
 source common.sh
 source settings.sh
 source fcs_genome.sh
-
-
-@test "args check" {
-  (($# == 0)) || (($# == 1))
-}
+source fcs_test_mode.sh
 
 @test "fcs-genome installed, PATH" {
   which fcs-genome &> /dev/null
@@ -17,40 +13,34 @@ source fcs_genome.sh
 db_dir=/curr/wenp/Analysis/fcs-genome_test-env
 db_samples=("CDMD1015")
 tmp_dir=/curr/wenp/Analysis/fcs-genome_test-env
-
-@test "set tool_mode" {
-  if [[ ! $# == 1 ]]; then
-  skip
-  fi
-  tool_mode=$1
-}
+log_file="${tmp_dir}/fcs_test.log"
 
 start_ts=$(date +%s)
 for sample in ${db_samples[@]}; do
 
   @test "${sample} align" {
-    if [[ (! -z $tool_mode) && ($tool_mode -ne "align") ]]; then
+    if [[ (! -z $tool_mode) && ($tool_mode != "align") ]]; then
       skip
     fi
     fcs_genome_align ${sample} "${tmp_dir}/${sample}-align.log"
   }
 
   @test "${sample} markdup" {
-    if [[ (! -z $tool_mode) && ($tool_mode -ne "markdup") ]]; then
+    if [[ (! -z $tool_mode) && ($tool_mode != "markdup") ]]; then
       skip
     fi
     fcs_genome_markdup ${sample} "${tmp_dir}/${sample}-markdup.log"
   }
 
   @test "${sample} bqsr" {
-    if [[ (! -z $tool_mode) && ($tool_mode -ne "bqsr") ]]; then
+    if [[ (! -z $tool_mode) && ($tool_mode != "bqsr") ]]; then
       skip
     fi
     fcs_genome_bqsr ${sample} "${tmp_dir}/${sample}-bqsr.log"
   }
 
   @test "${sample} htc" {
-    if [[ (! -z $tool_mode) && ($tool_mode -ne "htc") ]]; then
+    if [[ (! -z $tool_mode) && ($tool_mode != "htc") ]]; then
       skip
     fi
     fcs_genome_htc ${sample} "${tmp_dir}/${sample}-htc.log"
@@ -62,12 +52,15 @@ for sample in ${db_samples[@]}; do
     fi
     run $vcfdiff ${db_dir}/${sample}.vcf.gz ${tmp_dir}/${sample}.vcf.gz
     [ $status == 0 ]
-    [ eval 'res=(${lines[0])}' && $(echo "${res[5]} > 0.995" | bc) -eq 1 ]
+    precision=$(echo "${lines[1]}"| awk '{print $NF}')
+    ptest=$(echo "${precision} > 0.995" | bc)
+    [ "$ptest" -eq 1 ]
   }
-  
-  #rm -rf ${tmp_dir}/${sample}
-  #rm -rf ${tmp_dir}/${sample}.bam
-  #rm -rf ${tmp_dir}/${sample}.recal.bam
+  @test "file clean" {
+    rm -rf ${tmp_dir}/${sample}
+    rm -rf ${tmp_dir}/${sample}.bam
+    rm -rf ${tmp_dir}/${sample}.recal.bam
+  }
 done
 
 end_ts=$(date +%s)
---
+++ test/weekly/fcs_test_mode.sh
@@ -0,0 +1,1 @@
+tool_mode="align"
