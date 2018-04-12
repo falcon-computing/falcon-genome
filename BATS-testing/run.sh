d5288740d791c21e63e6fcee500ed6f0ba4f8607
Niveda-S <nivedasundar05@gmail.com>
2018-04-11 18:01:14 -0700

    Minor updates

--- BATS-testing/run.sh
+++ BATS-testing/run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
 source settings.bash
 
 # Create directory
-#mkdir -p $test_fastq
 mkdir -p $output_dir
-
-# Download small FASTQ files from s3
-#aws s3 sync s3://fcs-genome-data/falcon-genome-test/data/daily/fastq/ "$test_fastq"
 
 # Install BATS
 mkdir -p "${DIR}/env"
