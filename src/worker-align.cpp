ee2da022c8f8184df7bdc5f621c1a6edf6dfe396
Alfonso Lam <arlamng74@Alfonsos-MacBook-Pro.local>
2018-06-01 09:51:26 -0700

    fixing bug for output_path

--- src/worker-align.cpp
+++ src/worker-align.cpp
@@ -189,6 +189,7 @@ int align_main(int argc, char** argv,
               }
               parts_dir = temp_dir + "/" +
               get_basename(BAMfile) + ".parts";
+              output_path=BAMfile;
            }
 
         }
