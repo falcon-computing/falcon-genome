a74e5e4cab51ce5b9e97cfda9bdfa36eae45fa68
arlamng74 <alfonso@falcon-computing.com>
2018-06-01 20:09:22 -0700

    Another iteration to handle multiple fastq files for 1 sample

--- src/worker-align.cpp
+++ src/worker-align.cpp
@@ -241,9 +241,17 @@ int align_main(int argc, char** argv,
         output_path = temp;
 
         // Remove parts_dir
-        if (list.size() >1) parts_dir = temp + "/" + sample_id + "/*/";
-        remove_path(parts_dir);
-        DLOG(INFO) << "Removing temp file in '" << parts_dir << "'";
+        if (list.size() >1) {
+            for (int k = 0; k < list.size(); ++k) {
+                 parts_dir = temp + "/" + sample_id + "/" + list[i].ReadGroup;
+                 remove_path(parts_dir);
+                 DLOG(INFO) << "Removing temp file in '" << parts_dir << "'";
+            }
+        } else {
+            remove_path(parts_dir);
+            DLOG(INFO) << "Removing temp file in '" << parts_dir << "'";
+        }
+
     }
   }; // end loop for Index Map
   return 0;
