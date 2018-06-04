286281fedca580365ea442fa4bd20f23c5011fac
Alfonso Lam <arlamng74@Alfonsos-MacBook-Pro.local>
2018-06-04 09:41:49 -0700

    Unit Test for Sample Sheet

--- test/SampleSheet_UnitTest.cpp
+++ test/SampleSheet_UnitTest.cpp
@@ -1,45 +1,111 @@
-#include <limits.h>
-#include "fcs-genome/SampleSheet.h"
+
 #include "gtest/gtest.h"
-#include <string>
+#include "iostream"
+#include <limits.h>
+#include <stdexcept>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include <map>
+#include <vector>
+#include "fcs-genome/SampleSheet.h"
+#include "fcs-genome/SampleSheet_UnitTest.h"
 
 namespace fcs = fcsgenome;
+typedef std::map<std::string, std::vector<fcs::SampleDetails> > SampleSheetMap;
 
-// Defining Sample Sheet Name:
-std::string SampleSheetName = "SampleSheet.csv";
-fcs::SampleSheet MySampleSheet(SampleSheetName);
-std::string CheckName = MySampleSheet.get_fname();
-// Determine if Sample Sheet exists:
-int exist = 0;
-int CheckFile = MySampleSheet.check_file();
-// Determine if Sample Sheet is a File:
-bool IsFile = MySampleSheet.is_file();
-// Determine if Sample Sheet is a Folder:
-bool IsDir = MySampleSheet.is_dir();
+namespace {
 
-// Defining Map if Sample Sheet is a File:
-std::map<std::string, std::vector<fcs::SampleDetails> > SampleData = MySampleSheet.extract_data_from_file();
-// Defining Map if Sample Sheet is a Folder:
-std::map<std::string, std::vector<fcs::SampleDetails> > SampleData2 = MySampleSheet.extract_data_from_folder();
+class QuickTest : public testing::Test {
+ protected:
+  virtual void SetUp() {
+    start_time_ = time(NULL);
+  }
+  time_t start_time_;
+};
 
-// Running Test:
-TEST(SampleSheetTest, GetName) {
-  ASSERT_STREQ(SampleSheetName.c_str(),CheckName.c_str());
+class TestSampleSheetClass : public QuickTest {
+
+};
+
+std::string SampleSheetPath;
+SampleSheetMap SampleData; 
+std::string SAMPLE_ID[4] = {"sampleA", "sampleB", "sampleC", "sampleD"};
+std::string FASTQ_R1[4] = {"sampleA_r1.fastq.gz", "sampleB_r1.fastq.gz", "sampleC_r1.fastq.gz", "sampleD_r1.fastq.gz"};
+std::string FASTQ_R2[4] = {"sampleA_r2.fastq.gz", "sampleB_r2.fastq.gz", "sampleC_r2.fastq.gz", "sampleD_r2.fastq.gz"};
+std::string READ_GROUP[4] = {"ABC123", "ABC456", "ABC789", "ADD123"};
+std::string LIB[4] = {"RD001", "RD002" , "RD003", "RD004" };
+std::string PLATFORM = "Illumina";
+
+TEST_F(TestSampleSheetClass, CheckSampleSheet) {
+   SampleSheetPath="SampleSheet.csv";
+   create_file(SampleSheetPath);
+   fcs::SampleSheet MySampleSheet(SampleSheetPath); 
+   SampleData = MySampleSheet.get();
+   std::cout << "Check if SampleSheet.csv file is not empty (5 lines: 1 header and 4 inputs)" << std::endl;
+   EXPECT_EQ(0, SampleSheetPath.empty());
+   std::cout << "Check if SampleData Map is not empty" << std::endl;
+   EXPECT_EQ(1, !SampleData.empty());
+   std::cout << "Check if SampleData Map contains 4 inputs" << std::endl;
+   EXPECT_EQ(4, SampleData.size());
+   std::cout << "Check if each sample in SampleData Map contains 6 fields" << std::endl;
+   int count=0;
+   for (auto pair : SampleData) {
+     std::string sample_id = pair.first;
+     EXPECT_STREQ(SAMPLE_ID[count].c_str(), sample_id.c_str());
+     std::vector<fcs::SampleDetails> list = pair.second;
+     for(int i = 0; i < list.size(); ++i) {
+       std::string fastq1 = list[i].fastqR1;
+       std::string fastq2 = list[i].fastqR2;
+       std::string rg = list[i].ReadGroup;
+       std::string platform = list[i].Platform;
+       std::string library_id= list[i].LibraryID;
+       EXPECT_STREQ(FASTQ_R1[count].c_str(), fastq1.c_str());       
+       EXPECT_STREQ(FASTQ_R2[count].c_str(), fastq2.c_str());
+       EXPECT_STREQ(READ_GROUP[count].c_str(), rg.c_str());
+       EXPECT_STREQ(PLATFORM.c_str(), platform.c_str());
+       EXPECT_STREQ(LIB[count].c_str(), library_id.c_str());
+     }
+     count++;
+   }
+   remove_list(SampleSheetPath);
+   SampleData.clear();   
 }
 
-TEST(SampleSheetTest, FileExists) {
-  EXPECT_EQ(exist, CheckFile);
+
+TEST_F(TestSampleSheetClass, CheckSampleSheetPATH) {
+  SampleSheetPath="fastq/";
+  create_dir_with_data(SampleSheetPath);
+  fcs::SampleSheet MySampleSheet(SampleSheetPath);
+  SampleData = MySampleSheet.get();
+  std::cout << "Check if "<< SampleSheetPath << " folder is not empty (5 lines: 1 header and 4 inputs)" << std::endl;
+  EXPECT_EQ(0, SampleSheetPath.empty());
+  std::cout << "Check if SampleData Map is not empty" << std::endl;
+  EXPECT_EQ(1, !SampleData.empty());
+  std::cout << "Check if SampleData Map contains 4 inputs" << std::endl;
+  EXPECT_EQ(4, SampleData.size());
+  std::cout << "Check if each sample in SampleData Map contains 6 fields" << std::endl;
+  int count=0;
+  for (auto pair : SampleData) {
+    std::string sample_id = pair.first;
+    EXPECT_STREQ(SAMPLE_ID[count].c_str(), sample_id.c_str());
+    std::vector<fcs::SampleDetails> list = pair.second;
+    for(int i = 0; i < list.size(); ++i) {
+	std::string fastq1 = list[i].fastqR1;
+	std::string fastq2 = list[i].fastqR2;
+	std::string rg = list[i].ReadGroup;
+	std::string platform = list[i].Platform;
+	std::string library_id= list[i].LibraryID;
+	EXPECT_STREQ(FASTQ_R1[count].c_str(), fastq1.c_str());
+	EXPECT_STREQ(FASTQ_R2[count].c_str(), fastq2.c_str());
+	//EXPECT_STREQ(READ_GROUP[count].c_str(), rg.c_str());
+	//EXPECT_STREQ(PLATFORM.c_str(), platform.c_str());
+	//EXPECT_STREQ(LIB[count].c_str(), library_id.c_str());
+    }
+    count++;
+  }
+  remove_dir(SampleSheetPath);
+  SampleData.clear();
 }
 
-TEST(SampleSheetTest, IsDirOrNot) {
-  EXPECT_EQ(exist,IsDir);
-}
-
-TEST(SampleSheetTest, ExtractDataFromFile) {
-  EXPECT_EQ(1,SampleData.empty());
-}
-
-TEST(SampleSheetTest, ExtractDataFromFolder) {
-  EXPECT_EQ(1,SampleData2.empty());
-}
+}  // namespace
