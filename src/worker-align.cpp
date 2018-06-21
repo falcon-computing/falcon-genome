95989ccff4dcb5e3ceda3cd6ff6fca9e2fda4c26
Alfonso Lam <alfonso@falcon-computing.com>
2018-06-21 12:02:10 -0700

    check bwa log

--- src/worker-align.cpp
+++ src/worker-align.cpp
@@ -262,6 +262,7 @@ int align_main(int argc, char** argv,
         if (!sampleList.empty()) {
             std::string log_filename_md  = output_path + "/" + sample_id + "/" + sample_id + "_bwa.log";
             std::fstream bwa_log;
+            DLOG(INFO) << "Inside Loop " << log_filename_md;
             bwa_log.open(log_filename_md, std::fstream::app);
             bwa_log << sample_id << ":" << read_group << " : Start doing Mark Duplicates " << std::endl;
             bwa_log << sample_id << ":" << read_group << " : Mark Duplicates finishes in " << getTs() - start_markdup << " seconds" << std::endl;
