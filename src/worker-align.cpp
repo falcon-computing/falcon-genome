58d9c14ee955ef9ac91decc4f7a27bf25aa8475b
Alfonso Lam <alfonso@falcon-computing.com>
2018-06-26 13:44:38 -0700

    First Iteration

--- src/worker-align.cpp
+++ src/worker-align.cpp
@@ -288,28 +288,19 @@ int align_main(int argc, char** argv,
         cmd << get_config<std::string>("sambamba_path") << " merge "
             << "-l 1 "
             << "-t " << get_config<int>("markdup.nt") << " ";
-        if (list.size() >1) {
-            for (int m = 0; m < list.size(); m++) {
-                 parts_dir = output_path + "/" + sample_id + "/" + list[m].ReadGroup;
-                 std::vector<std::string> input_files_ ;
-                 get_input_list(parts_dir, input_files_, ".*/part-[0-9].*", true);
-                 for (int n = 0; n < input_files_.size(); n++) {
-                      cmd << input_files_[n] << " ";
-                 }
-            }
-        } else {
-            parts_dir = output_path + "/" + sample_id + "/" + list[m].ReadGroup;
-            std::vector<std::string> input_files_ ;
-            get_input_list(parts_dir, input_files_, ".*/part-[0-9].*", true);
-            for (int n = 0; n < input_files_.size(); n++) {
-                 cmd << input_files_[n] << " ";
-            }
-        }
-        std::string mergeBAM = output_path + "/" + sample_id + "/" + sample_id + ".bam";
-        cmd << mergeBAM;
-        DLOG(INFO) << "Merging Parts BAM Files for " << sample_id << std::endl;
-        cmd_ = cmd.str();
-        DLOG(INFO) << cmd_ << std::endl;
+        for (int m = 0; m < list.size(); m++) {
+             parts_dir = output_path + "/" + sample_id + "/" + list[m].ReadGroup;
+             std::vector<std::string> input_files_ ;
+             get_input_list(parts_dir, input_files_, ".*/part-[0-9].*", true);
+             for (int n = 0; n < input_files_.size(); n++) {
+                  cmd << input_files_[n] << " ";
+             }
+         }
+         std::string mergeBAM = output_path + "/" + sample_id + "/" + sample_id + ".bam";
+         cmd << mergeBAM;
+         DLOG(INFO) << "Merging Parts BAM Files for " << sample_id << std::endl;
+         cmd_ = cmd.str();
+         DLOG(INFO) << cmd_ << std::endl;
 
     }
 
