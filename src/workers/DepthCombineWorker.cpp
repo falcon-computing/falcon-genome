c0e90b7a329afd4aa91a14fb8328cecccc43bd89
arlamng74 <alfonso@falcon-computing.com>
2018-07-10 20:09:46 -0700

    DepthCombined: Minor Editing

--- src/workers/DepthCombineWorker.cpp
+++ src/workers/DepthCombineWorker.cpp
@@ -221,7 +221,7 @@ void DepthCombineWorker::merge_outputs(std::string file_type) {
               //fprintf(summary_file, "%s\t%d\t%.2f\t%d\t%d\t%d\t%.2f\n",
               //        sampleName, total_coverage, mean, cov_indexQ3,
               //        cov_indexQ2, cov_indexQ1, pct15x);
-              fprintf(summary_file, "%s\n", sampleName.c_str());
+              fprintf(summary_file, "%s\t%d\t%.2f\t%d\t%d\t%d\n", sampleName.c_str() ,total_coverage, mean, cov_indexQ3, cov_indexQ2, cov_indexQ1 );
               fclose(summary_file);
        } // sample_summary file generated
    }
