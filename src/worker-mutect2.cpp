2f12fb212138a69fb0a941b8355ea36053b8e251
arlamng74 <alfonso@falcon-computing.com>
2018-07-27 15:46:08 -0700

    First Iteration

--- src/worker-mutect2.cpp
+++ src/worker-mutect2.cpp
@@ -15,7 +15,7 @@
 namespace fcsgenome {
 
 int mutect2_main(int argc, char** argv,
-    boost::program_options::options_description &opt_desc) 
+    boost::program_options::options_description &opt_desc)
 {
   namespace po = boost::program_options;
 
@@ -23,24 +23,28 @@ int mutect2_main(int argc, char** argv,
   po::variables_map cmd_vm;
   bool opt_bool = false;
 
-  opt_desc.add_options() 
+  opt_desc.add_options()
     ("ref,r", po::value<std::string>()->required(), "reference genome path")
     ("normal,n", po::value<std::string>()->required(), "input normal BAM file or dir")
-    ("tumor,t", po::value<std::string>()->required(), "input tumor BAM file or dir")
+    //("tumor,t", po::value<std::string>()->required(), "input tumor BAM file or dir")
+    ("tumor,t", po::value<std::string>(), "input tumor BAM file or dir")
     ("output,o", po::value<std::string>()->required(), "output VCF file")
-    ("dbsnp,d", po::value<std::vector<std::string> >(), "list of dbsnp files for Mutect2")
-    ("cosmic,c", po::value<std::vector<std::string> >(), "list of cosmic files for Mutect2")
-    ("intervalList,L", po::value<std::vector<std::string> >(), "interval list file")
+    //("dbsnp,d", po::value<std::vector<std::string> >(), "list of dbsnp files for Mutect2")
+    //("cosmic,c", po::value<std::vector<std::string> >(), "list of cosmic files for Mutect2")
+    //("intervalList,L", po::value<std::vector<std::string> >(), "interval list file")
+    ("dbsnp,d", po::value<std::string>(), "list of dbsnp files for Mutect2")
+    ("cosmic,c", po::value<std::string>(), "list of cosmic files for Mutect2")
+    ("intervalList,L", po::value<std::string>(), "interval list file")
     ("skip-concat,s", "produce a set of VCF files instead of one");
-    
+
   // Parse arguments
   po::store(po::parse_command_line(argc, argv, opt_desc),
       cmd_vm);
 
-  if (cmd_vm.count("help")) { 
+  if (cmd_vm.count("help")) {
     throw helpRequest();
-  } 
-  
+  }
+
   // Check configurations
   check_nprocs_config("mutect2");
   check_memory_config("mutect2");
@@ -55,7 +59,7 @@ int mutect2_main(int argc, char** argv,
   std::vector<std::string> dbsnp_path = get_argument<std::vector<std::string> >(cmd_vm, "dbsnp", "d", std::vector<std::string>());
   std::vector<std::string> cosmic_path = get_argument<std::vector<std::string> >(cmd_vm, "cosmic","c", std::vector<std::string>());
   std::vector<std::string> intv_list = get_argument<std::vector<std::string> >(cmd_vm, "intervalList", "L");
-  std::vector<std::string> extra_opts = get_argument<std::vector<std::string>>(cmd_vm, "extra-options", "O"); 
+  std::vector<std::string> extra_opts = get_argument<std::vector<std::string>>(cmd_vm, "extra-options", "O");
 
   // finalize argument parsing
   po::notify(cmd_vm);
@@ -75,7 +79,35 @@ int mutect2_main(int argc, char** argv,
   create_dir(output_dir);
 
   std::vector<std::string> output_files(get_config<int>("gatk.ncontigs"));
-  std::vector<std::string> intv_paths = init_contig_intv(ref_path);
+  //std::vector<std::string> intv_paths = init_contig_intv(ref_path);
+  std::vector<std::string> intv_paths;
+
+  std::map<size_t, std::vector<std::string> > dbSNP_sets;
+  std::map<size_t, std::vector<std::string> > cosmic_sets;
+
+  if (!intv_list.empty()) {
+     DLOG(INFO) << "Interval list defined";
+     intv_paths = split_by_nprocs(intv_list, "bed");
+  } else {
+     DLOG(INFO) << "Interval list not defined. Coordinates from Reference will be used for MuTect2";
+     intv_paths = split_ref_by_nprocs(ref_path);
+  }
+
+  if (!dbsnp_path.empty()){
+      std::vector<std::string> tmp_dnsp;
+      for (size_t i = 0; i < dbsnp_path.size(); i++){
+           tmp_dnsp = split_by_nprocs(dbsnp_path[i], "vcf");
+           dbSNP_sets.insert(pair<size_t, std:vector<std::string>>(i,tmp_dbsnp));
+      }
+
+  }
+
+  if (!cosmic_path.empty()){
+      std::vector<std::string> tmp_cosmic;
+
+  }
+
+
 
   // start an executor for NAM
   Worker_ptr blaze_worker(new BlazeWorker(
@@ -84,10 +116,9 @@ int mutect2_main(int argc, char** argv,
 
   BackgroundExecutor bg_executor("blaze-nam", blaze_worker);
 
-
   Executor executor("Mutect2", get_config<int>("gatk.mutect2.nprocs"));
-  
-  bool flag_mutect2_f = !flag_skip_concat | flag_f;  
+
+  bool flag_mutect2_f = !flag_skip_concat | flag_f;
   for (int contig = 0; contig < get_config<int>("gatk.ncontigs"); contig++) {
     std::string normal_file;
     std::string tumor_file;
@@ -119,7 +150,7 @@ int mutect2_main(int argc, char** argv,
 
     executor.addTask(worker);
   }
-  
+
   if (!flag_skip_concat) {
 
     bool flag = true;
@@ -130,7 +161,7 @@ int mutect2_main(int argc, char** argv,
             flag_a, flag));
       executor.addTask(worker, true);
     }
-    
+
     { // bgzip gvcf
       Worker_ptr worker(new ZIPWorker(
         temp_gvcf_path, output_path+".gz",
