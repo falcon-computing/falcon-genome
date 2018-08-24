1b571b17bcd38fe3128c94525512f6cbb2409958
Di Wu <allwu@falcon-computing.com>
2018-08-23 21:48:33 -0700

    fix the same issue for mutect2

--- include/fcs-genome/workers/Mutect2Worker.h
+++ include/fcs-genome/workers/Mutect2Worker.h
@@ -18,7 +18,6 @@ class Mutect2Worker : public Worker {
       std::vector<std::string> &dbsnp_path,
       std::vector<std::string> &cosmic_path,
       std::string &germline_path,
-      std::string &intv_list,
       int contig,
       bool &flag_f, bool flag_gatk);
 
@@ -34,7 +33,6 @@ class Mutect2Worker : public Worker {
   std::vector<std::string> &dbsnp_path_;
   std::vector<std::string> &cosmic_path_;
   std::string &germline_path_;
-  std::string intv_list_;
   bool flag_gatk_;
 };
 } // namespace fcsgenome
