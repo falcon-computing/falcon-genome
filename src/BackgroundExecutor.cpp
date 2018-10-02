e0d330702c0bb4343351ca9eb37a7dde29a8d30c
arlamng74 <alfonso@falcon-computing.com>
2018-10-02 12:10:57 -0700

    ../src/BackgroundExecutor.cpp updated

--- src/BackgroundExecutor.cpp
+++ src/BackgroundExecutor.cpp
@@ -19,8 +19,6 @@ BackgroundExecutor::BackgroundExecutor( std::string job_name, Worker_ptr worker)
 
   create_dir(get_config<std::string>("log_dir"));
 
-  std::string tag = worker->getTaskName();
-
   std::string log = get_log_name(job_name);
   std::string cmd = worker->getCommand() + " 1> /dev/null" + " 2> " + log;
   LOG(INFO) << "BackgroundExecutor cmd : " << cmd;
