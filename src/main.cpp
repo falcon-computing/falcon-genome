5602721172efc586ab6327f2a24780ef5ef308c7
Alfonso Lam <37307227+arlamng74@users.noreply.github.com>
2018-11-26 10:20:57 -0800

    Gen 622 variant filter support (#163)

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,7 @@ int print_help() {
   print_cmd_col("ug", "variant calling with GATK UnifiedGenotyper");
   print_cmd_col("gatk", "call GATK routines");
   print_cmd_col("depth", "Depth of Coverage");
+  print_cmd_col("vcf_filter", "Variant Filtration");
 
   return 0;
 }
@@ -68,6 +69,7 @@ namespace fcsgenome {
   int hist_main(int argc, char** argv, po::options_description &opt_desc);
   int mutect2_main(int argc, char** argv, po::options_description &opt_desc);
   int depth_main(int argc, char** argv, po::options_description &opt_desc);
+  int variant_filtration_main(int argc, char** argv, po::options_description &opt_desc);
 }
 
 int main(int argc, char** argv) {
@@ -157,6 +159,9 @@ int main(int argc, char** argv) {
     }
     else if (cmd == "depth") {
       depth_main(argc-1, &argv[1], opt_desc);
+    }
+    else if (cmd == "vcf_filter") {
+      variant_filtration_main(argc-1, &argv[1], opt_desc);
     }
     else if (cmd == "--version") {
       std::cout << VERSION << std::endl;
