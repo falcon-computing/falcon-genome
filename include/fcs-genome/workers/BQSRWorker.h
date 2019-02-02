3cc74bc904b340898ec6dae39557724287b1755d
Alfonso Lam <37307227+arlamng74@users.noreply.github.com>
2019-02-01 16:31:43 -0800

    fix build issue caused by gen 802 (#227)

--- include/fcs-genome/workers/BQSRWorker.h
+++ include/fcs-genome/workers/BQSRWorker.h
@@ -51,7 +51,7 @@ class PRWorker : public Worker {
   PRWorker(std::string ref_path,
       std::vector<std::string> intv_path,
       std::string bqsr_path,
-      std::vector<std::string> input_path,
+      std::string input_path,
       std::string output_path,
       std::vector<std::string> extra_opts,
       int contig, bool &flag_f, bool flag_gatk);
