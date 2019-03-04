bacf1eab3e52bb975b6404e0b9c55e471d895a02
Di Wu <allwu@falcon-computing.com>
2019-03-04 12:00:27 -0800

    fix unit test init issue; add check in jenkins to test if branch is behind release

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,17 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 
+#include "fcs-genome/config.h"
+
 class TestConfig;
 
 int main(int argc, char *argv[]) {
   google::InitGoogleLogging(argv[0]);
   FLAGS_logtostderr = true;
   ::testing::InitGoogleTest(&argc, argv);
+
+  // initialize configurations
+  fcsgenome::init(argv, argc); 
 
   // run all tests
   return RUN_ALL_TESTS();
