5079903e97a1c697065a76486f8bbddd26005ca1
arlamng74 <alfonso@falcon-computing.com>
2019-03-05 13:53:58 -0800

    worker-germline.cpp : minor change in the Sambamba SORT block

--- src/worker-germline.cpp
+++ src/worker-germline.cpp
@@ -204,7 +204,7 @@ int germline_main(int argc, char** argv, boost::program_options::options_descrip
           std::string input = get_bucket_fname(parts_dir, i);
           bool flag = true;
           Worker_ptr worker(new SambambaWorker(
-                input, "",
+                input, input,
                 SambambaWorker::SORT,
                 "", flag)); 
           executor.addTask(worker, sample_id, i == 0);
